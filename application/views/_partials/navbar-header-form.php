f58bfc6ce89c9b8acacca841c65250963b04f1fb
Rum <muhammadrum04@gmail.com>
2021-06-01 09:59:25 +0700

    goo

--- application/views/_partials/navbar-header-form.php
+++ application/views/_partials/navbar-header-form.php
@@ -3,7 +3,7 @@
     <div class="navbar-nav flex-row order-md-last">
         <div class="nav-item dropdown">
         <a href="javascript:void(0)" class="nav-link d-flex lh-1 text-reset p-0" aria-label="Open user menu">
-            <span class="avatar avatar-sm" style="background-image: url(<?= base_url()?>assets/img/logo.png)"></span>
+            <span class="avatar avatar-sm" style="background-image: url(<?= base_url()?>assets/img/logo.jpg)"></span>
         </a>
         </div>
     </div>
